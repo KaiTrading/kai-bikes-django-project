028fa11d83ab85b0138c1fe711137fdac94fbe98
KaiTrading <kaitrading.host@gmail.com>
2023-01-14 13:57:53 +0530

    debug

--- Kai/settings.py
+++ Kai/settings.py
@@ -167,6 +167,7 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- home/models.py
+++ home/models.py
@@ -11,7 +11,7 @@ class Site_Profile(models.Model):
         ('False', 'False'),
     )
     title = models.CharField(max_length=150)
-    logo = CloudinaryField('logo',folder = "logo",blank=True)
+    logo = models.ImageField(blank=True, upload_to='root/')
     keywords = models.CharField(max_length=255,blank=True)
     description = models.CharField(max_length=255,blank=True)
     url = models.CharField(max_length=255,blank=True)
